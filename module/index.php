0f43418ad0e24b838709d241ecb45d8007001307
QuanDepTrai612002 <ducquan15234@gmail.com>
2023-12-17 19:36:57 +0700

    show product

---
+++ module/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$ac = Utils::getIndex('ac', 'home');
+$product = new Product();
+$order = new Order();
+$detail = new DetailOrder();
+$category = new Category();
+$user = new User();
+if ($ac == 'home')
+{
+    include ROOT . '/module/showProduct.php';
+}
+

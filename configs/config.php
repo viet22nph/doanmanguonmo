b6bdfdf3e8d37d584220cd8ee57da967d796705a
QuanDepTrai612002 <ducquan15234@gmail.com>
2023-12-17 19:16:30 +0700

    file config

---
+++ configs/config.php
@@ -0,0 +1,12 @@
+<?php
+$configDB = array();
+$configDB["host"] 		= "localhost";
+$configDB["database"]	= "db_web_noi_that";
+$configDB["username"] 	= "root";
+$configDB["password"] 	= "";
+define("HOST", "localhost");
+define("DB_NAME", "db_web_noi_that");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define('ROOT', dirname(dirname(__FILE__) ) );
+define("BASE_URL", "http://".$_SERVER['SERVER_NAME']."/doanthaybang/");
